aaa91b273acde4f9f02a996c3d7c618412d28366
scadler <scadler@s.sfusd.edu>
2020-04-05 18:10:53 +0000

    added time hex

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ function changeColors(){
     $("#counterHex").text(hex)
     i++
     i = i%3375
+    var time = Math.floor((Date.now()/1000)%16581375
+).toString(16)
+    if(time.length === 1 ){
+        time= "00000"+time
+    }
+    else if(time.length === 2 ){
+        time = "0000"+time
+    }
+    if(time.length === 3 ){
+        time= "000"+time
+    }
+    else if(time.length === 4 ){
+        time = "00"+time
+    }
+    if(time.length === 5 ){
+        time= "0"+time
+    }
+    time = "#"+time
+    $("#clock").css("background-color",`${time}`)
+    $("#timeHex").text(time)
+    console.log(time)
 }
 setInterval(changeColors,30)
-
