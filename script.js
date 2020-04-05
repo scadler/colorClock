1faf4c5937568b42506e83cbcbe9b6c34567352f
scadler <scadler@s.sfusd.edu>
2020-04-05 18:14:25 +0000

    both colors now full hex values

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ var i = 0
 function changeColors(){
     hex = i.toString(16)
     if(hex.length === 1 ){
-        hex= "00"+hex
+        hex= "00000"+hex
     }
     else if(hex.length === 2 ){
-        hex = "0"+hex
+        hex = "0000"+hex
+    }
+    if(hex.length === 3 ){
+        hex= "000"+hex
+    }
+    else if(hex.length === 4 ){
+        hex = "00"+hex
+    }
+    else if(hex.length === 5 ){
+        hex= "0"+hex
     }
     hex = "#"+hex
     $("#counter").css("background-color",`${hex}`)
     $("#counterHex").text(hex)
     i++
-    i = i%3375
+    i = i%16581375
     var time = Math.floor((Date.now()/1000)%16581375
 ).toString(16)
     if(time.length === 1 ){
@@ -20,13 +29,13 @@ function changeColors(){
     else if(time.length === 2 ){
         time = "0000"+time
     }
-    if(time.length === 3 ){
+    else if(time.length === 3 ){
         time= "000"+time
     }
     else if(time.length === 4 ){
         time = "00"+time
     }
-    if(time.length === 5 ){
+    else if(time.length === 5 ){
         time= "0"+time
     }
     time = "#"+time
@@ -34,4 +43,4 @@ function changeColors(){
     $("#timeHex").text(time)
     console.log(time)
 }
-setInterval(changeColors,30)
+setInterval(changeColors,10)
