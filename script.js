da0e0345c06d3ce047aee5ba2cc23548204047e3
scadler <scadler@s.sfusd.edu>
2020-04-07 21:07:14 +0000

    completely redid clock

--- script.js
+++ script.js
@@ -1,46 +1,23 @@
-var i = 0
 function changeColors(){
-    hex = i.toString(16)
-    if(hex.length === 1 ){
-        hex= "00000"+hex
-    }
-    else if(hex.length === 2 ){
-        hex = "0000"+hex
-    }
-    if(hex.length === 3 ){
-        hex= "000"+hex
-    }
-    else if(hex.length === 4 ){
-        hex = "00"+hex
-    }
-    else if(hex.length === 5 ){
-        hex= "0"+hex
-    }
-    hex = "#"+hex
-    $("#counter").css("background-color",`${hex}`)
-    $("#counterHex").text(hex)
-    i++
-    i = i%16581375
-    var time = Math.floor((Date.now()/1000)%16581375
-).toString(16)
-    if(time.length === 1 ){
-        time= "00000"+time
-    }
-    else if(time.length === 2 ){
-        time = "0000"+time
-    }
-    else if(time.length === 3 ){
-        time= "000"+time
-    }
-    else if(time.length === 4 ){
-        time = "00"+time
-    }
-    else if(time.length === 5 ){
-        time= "0"+time
-    }
-    time = "#"+time
-    $("#clock").css("background-color",`${time}`)
-    $("#timeHex").text(time)
-    console.log(time)
+    var date = new Date()
+    hours = (date.getHours() < 10) ? "0"+date.getHours() : date.getHours()
+    minutes = (date.getMinutes() < 10) ? "0"+date.getMinutes() : date.getMinutes()
+    seconds = (date.getSeconds() < 10) ? "0"+date.getSeconds() : date.getSeconds()
+    // milliseconds = date.getMilliseconds()
+    // secondsSinceMidnight = hours*3600 + minutes*60 + seconds
+    // secondsInADay = 86400
+    hexHours = (Math.floor(255 * hours/24).toString(16).length > 1) ?  Math.floor(255 * hours/24).toString(16) : "0" + Math.floor(255 * hours/24).toString(16)
+    hexMinutes = (Math.floor(255 * minutes/60).toString(16).length > 1) ? Math.floor(255 * minutes/60).toString(16): "0" + Math.floor(255 * minutes/60).toString(16)
+    hexSeconds = (Math.floor(255 * seconds/60).toString(16).length > 1) ? Math.floor(255 * seconds/60).toString(16): "0" + Math.floor(255 * seconds/60).toString(16) 
+    // console.log(secondsSinceMidnight +"   "+ secondsSinceMidnight%60)
+    $("#time").text(hours + ":" + minutes + ":" + seconds)
+    $("html").css("background-color","#"+`${hexHours}`+`${hexMinutes}`+`${hexSeconds}`)
+    $("#timeHex").text("#"+`${hexHours}`+`${hexMinutes}`+`${hexSeconds}`)
+    $("#hour").css("background-color","#"+`${hexHours}`+"0000")
+    $("#hourHex").text("#"+`${hexHours}`+"0000")
+    $("#minute").css("background-color","#00"+`${hexMinutes}`+"00")
+    $("#minuteHex").text("#00"+hexMinutes+"00")
+    $("#second").css("background-color","#0000"+`${hexSeconds}`)
+    $("#secondHex").text("#0000"+hexSeconds)
 }
-setInterval(changeColors,10)
+setInterval(changeColors,1000)
